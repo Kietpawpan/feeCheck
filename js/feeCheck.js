9ea3ed0fe08ab28799ac4382a43c8f299f03d592
jTech <kietpawpan@gmail.com>
2023-06-22 12:11:56 +0700

    Add files via upload

---
+++ js/feeCheck.js
@@ -0,0 +1,11 @@
+/* Copyright (c) 2023 Montri Kiatphaophan
+ * login.js | All rights reserved.
+*/
+
+
+function login() {
+	var id = document.getElementById('did').value;
+	if(id == 'AA8-888-888') {window.open('html/AA8-888-888.html');}
+	else{alert('Incorrect ID. Please try again.');}
+}
+
