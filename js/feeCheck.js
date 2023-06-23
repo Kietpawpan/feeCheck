837c97248785209ef18ad158b41782f252d1a64d
jTech <kietpawpan@gmail.com>
2023-06-23 09:24:24 +0700

    Update feeCheck.js

--- js/feeCheck.js
+++ js/feeCheck.js
@@ -5,7 +5,7 @@
 
 function login() {
 	var id = document.getElementById('did').value;
-	if(id == 'AA8-888-888') {window.open('https://slc.mnre.go.th/th/news/detail/158179/');}
+	if(id == 'PP2303141345') {window.open('https://slc.mnre.go.th/th/news/detail/158179/');}
 	else{alert('Incorrect ID. Please try again.');}
 }
 
